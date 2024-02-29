056b7d76839e2a626d2e14e5e8567856c7266b8d
Ivan Poplayhin <poplayhin2002@gmail.com>
2024-02-29 19:12:09 +0500

    v1

---
+++ final_win.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+      
+from instr import *
+
+
+class FinalWin(QWidget):
+    def __init__(self):
+        ''' окно, в котором проводится опрос '''
+        super().__init__()
+
+
+        # создаём и настраиваем графические элементы:
+        self.initUI()
+
+
+        #устанавливает, как будет выглядеть окно (надпись, размер, место)
+        self.set_appear()
+        
+        # старт:
+        self.show()
+
+
